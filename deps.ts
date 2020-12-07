614d0fc81d9dfb3096430f189b3d4ee31dd0f180
Michael Maysonet <michaelmaysonet74@gmail.com>
2020-12-06 21:27:25 -0500

    initial commit

---
+++ deps.ts
@@ -0,0 +1,4 @@
+export {
+    Application as OakApp,
+    Router as OakRouter,
+} from "https://deno.land/x/oak/mod.ts";
---
+++ server.ts
@@ -0,0 +1,38 @@
+import { OakApp, OakRouter } from "./deps.ts";
+
+interface Deno {
+    name: string;
+    type: string;
+    version: string;
+    createdBy: string;
+    releaseDate: Date;
+}
+
+const deno: Deno = {
+    name: "Deno",
+    type: "JS/TS Runtime",
+    version: "1.5.2",
+    createdBy: "Ryan Dahl",
+    releaseDate: new Date("2020.11.09"),
+};
+
+const denos: Deno[] = [deno];
+
+const app = new OakApp();
+const router = new OakRouter();
+
+router.get("/api/deno", (ctx) => {
+    ctx.response.body = denos;
+});
+
+router.get("/api/deno/:version", (ctx) => {
+    const { version } = ctx.params;
+    ctx.response.body = denos.find(d => d.version === version);
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+const PORT = 8080;
+console.log(`Serving on http://localhost:${PORT}`);
+await app.listen({ port: PORT });
